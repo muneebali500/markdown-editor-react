bde0c94ec50c447108043d1f0a10bb6d4ff1e914
Muneeb Ali <muneebali500@gmail.com>
2022-10-12 21:23:03 +0500

    make wbpage responsive

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
-import { useState, useEffect } from "react";
+import React, { useState, useEffect } from "react";
 import ReactMarkdown from "react-markdown";
 
 import "./App.css";
 
 function App() {
   const [markdown, setMarkdown] = useState(``);
+  const [targetValue, setTargetValue] = useState(`editor`);
 
   function handleChange(e) {
     setMarkdown(e.target.value);
@@ -21,17 +22,27 @@ function App() {
       <header className="header">Markdown Editor</header>
       <main className="main">
         <section className="divider">
-          <h2 className="head2">Editor</h2>
+          <h2 onClick={() => setTargetValue(`editor`)} className="head2">
+            Editor
+          </h2>
           <textarea
             onChange={handleChange}
             className="editor"
             value={markdown}
-            placeholder="Place your text here..."
+            placeholder="Place your markdown content here..."
+            style={{ zIndex: `${targetValue === `editor` ? 1 : 0}` }}
           ></textarea>
         </section>
         <section className="divider">
-          <h2 className="head2">Result</h2>
-          <ReactMarkdown className="result">{markdown}</ReactMarkdown>
+          <h2 className="head2" onClick={() => setTargetValue(`result`)}>
+            Result
+          </h2>
+          <ReactMarkdown
+            className="result"
+            style={{ zIndex: `${targetValue === `result` ? 1 : 0}` }}
+          >
+            {markdown ? markdown : null}
+          </ReactMarkdown>
         </section>
       </main>
     </>
